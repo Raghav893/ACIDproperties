655e0d189ab131fe1c757ab7084a94821abbbf21
Raghav893 <raghavarora2309@gmail.com>
2026-01-09 12:15:37 +0530

    Initial commit

---
+++ src/main/java/com/raghav/acidproperties/AciDpropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.acidproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AciDpropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AciDpropertiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raghav/acidproperties/AciDpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.acidproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AciDpropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
